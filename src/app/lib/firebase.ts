067b891148248c95e0fcf569f575c9aca7d0a7c6
Murilo Stein <murilo.r.stein@gmail.com>
2025-01-21 17:51:34 -0300

    feat: authentication with google and link creation

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -4,14 +4,13 @@ import { getStorage } from "firebase-admin/storage";
 import "server-only";
 
 /*           CERTIFICADO                  */
-const decodedKey = Buffer.from(
-  process.env.FIREBASE_PRIVATE_KEY!,
-  "base64"
-).toString("utf-8");
+const privateKey =
+  process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "";
+
 export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: decodedKey,
+  privateKey: privateKey,
 });
 
 /*       INSTÂNCIA DO APP            */
