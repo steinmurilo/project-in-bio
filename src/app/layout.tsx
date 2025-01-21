8a1bdf7cf74cc48e6df8cbadae292c89fbb806f6
Murilo Stein <murilo.r.stein@gmail.com>
2025-01-21 11:44:16 -0300

    add landing-page/Hero and ui components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import "./globals.css";
 
+import { Red_Hat_Display } from 'next/font/google'
+
+const redHatDisplay = Red_Hat_Display({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+})
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`antialiased`}>
+      <body className={`${redHatDisplay} antialiased bg-background-primary text-content-body`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+import Hero from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-background-primary">Hello World</div>
+    <div className='max-w-7xl mx-auto border'>
+      <Hero />
+      {/* <Header />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
+    </div>
   );
 }
